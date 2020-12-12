822b94389c2d6faa11dd76eb4824952348b16ea0
Ankit Gupta <gankit389@gmail.com>
2020-12-12 18:29:00 -0500

    firbase integreation done

--- lamp/data.py
+++ lamp/data.py
@@ -1,1 +1,15 @@
 import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate("admin_key.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL' : 'https://sfasdf-e46a2-default-rtdb.firebaseio.com/'
+})
+
+root = db.reference() 
+def main():
+    my_dict = root.get('/')[0]
+    final_string = str(my_dict['red'])+","+str(my_dict['green'])+","+str(my_dict['blue'])+","+my_dict['message']
+    print(final_string)
+main()
--- lamp/main.py
+++ lamp/main.py
@@ -2,15 +2,27 @@ from serial import Serial
 import random
 import time 
 import data
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
 
 port = Serial('COM3', 9600)
 port.close()
 port.open()
-while True:
-    redval = str(250)
-    greenval = str(0)
-    blueval = str(0)
-    message = "hello"
-    final_string  = redval+","+greenval+","+blueval+","+message
+cred = credentials.Certificate("admin_key.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL' : 'https://sfasdf-e46a2-default-rtdb.firebaseio.com/'
+})
+
+root = db.reference() 
+
+def main():
+    my_dict = root.get('/')[0]
+    final_string = str(my_dict['red'])+","+str(my_dict['green'])+","+str(my_dict['blue'])+","+my_dict['message']
     port.write(str.encode(final_string))
+
+
+while True:
+    main()
     time.sleep(2)
